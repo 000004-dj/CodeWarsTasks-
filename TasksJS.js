397167f3ea8bec0c6bd93f40f197c8768222ae51
Samuil <samuil-18@tut.by>
2022-10-07 22:36:43 +0200

    add task 2

--- TasksJS.js
+++ TasksJS.js
@@ -1,1 +1,19 @@
 console.log('hello, world')
+
+//task 2 
+let a = 1
+let b = 2 
+let c = 3
+function expressionMatter(a, b, c) {
+  let r = Math.max( 
+       a+b+c,
+        a*b*c,
+        a*b+c,
+        a*(b+c),
+        a+b*c,
+        (a+b)*c
+    );
+  return r // highest achievable result
+}
+
+//task 2 
