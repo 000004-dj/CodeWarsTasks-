8c780d2d04980a0abfda6346a3ee2c51c65d61ab
Samuil <samuil-18@tut.by>
2022-10-17 19:13:49 +0200

    add new task

--- TasksJS.js
+++ TasksJS.js
@@ -283,12 +283,39 @@
 //     };
 //   }
 
-function saleHotdogs(n){
-  if(n < 5) {
-    return n * 100
-  }else if(n >= 5 && n < 10){
-    return n * 95
-  } else if(n >= 10){
-    return n * 90
+// function saleHotdogs(n){
+//   if(n < 5) {
+//     return n * 100
+//   }else if(n >= 5 && n < 10){
+//     return n * 95
+//   } else if(n >= 10){
+//     return n * 90
+//   }
+// }
+
+
+
+function howManydays(month){
+  var days = month;
+  switch (days){
+  case 1:
+    case 3:
+      case 5:
+        case 7:
+          case 8:
+            case 10:
+              case 12:
+                  days = 31;
+                            break;
+  case 4:
+    case 6:
+      case 9:
+        case 11:
+            days = 30;
+                      break;
+  case 2:
+      days = 28;
+                break;
   }
+  return days;
 }
