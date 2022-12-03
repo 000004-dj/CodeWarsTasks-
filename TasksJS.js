04c657c5d86c85771618f3d196bc344aa0578f22
Samuil <samuil-18@tut.by>
2022-12-03 20:53:20 +0100

    add new task from codewars

--- TasksJS.js
+++ TasksJS.js
@@ -475,37 +475,41 @@
 // }
 
 
-function switchItUp(number){
-    switch (number){
-        case 1:
-            return "One";
-            break;
-        case 2:
-            return "Two"
-            break;
-        case 3:
-            return"Three"
-            break;
-        case 4:
-            return"Four"
-            break;
-        case 5:
-            return"Five"
-            break;
-        case 6:
-            return"Six"
-            break;
-        case 7:
-            return "Seven"
-            break;
-        case 8:
-            return "Eight"
-            break;
-        case 9:
-            return "Nine"
-            break;
-        default:
-            return "Zero"
-    }
-//Write your own Code!
+// function switchItUp(number){
+//     switch (number){
+//         case 1:
+//             return "One";
+//             break;
+//         case 2:
+//             return "Two"
+//             break;
+//         case 3:
+//             return"Three"
+//             break;
+//         case 4:
+//             return"Four"
+//             break;
+//         case 5:
+//             return"Five"
+//             break;
+//         case 6:
+//             return"Six"
+//             break;
+//         case 7:
+//             return "Seven"
+//             break;
+//         case 8:
+//             return "Eight"
+//             break;
+//         case 9:
+//             return "Nine"
+//             break;
+//         default:
+//             return "Zero"
+//     }
+// //Write your own Code!
+// }
+
+function solution(str){
+    return str.split("").reverse().join("");
 }
